c55390fb8714f7a01e2a9e6da9039ec7a4d13541
AndresApolo10 <andres-10a@hotmail.com>
2022-07-08 22:36:11 -0500

    ecommerce

---
+++ main.js
@@ -0,0 +1,16 @@
+const iconBurger = document.querySelector("#icon_burger");
+const menu = document.querySelector("#menu");
+const iconCerrar = document.querySelector("#icon_cerrar");
+
+iconBurger.addEventListener("click", function (){
+    menu.classList.toggle("show__menu");
+});
+
+iconCerrar.addEventListener("click", function (){
+    menu.classList.toggle("show__menu");
+});
+
+
+
+
+
